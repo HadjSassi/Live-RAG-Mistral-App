b1f5628d80f75235197391fcd47632fea8c1fe31
HADJ SASSI Mahdi <mahdi.hadjsassi@digitalseeder.com>
2025-05-30 10:14:26 +0200

    Ignore .idea, __pycache__, and env.py

--- env.py
+++
@@ -1,6 +0,0 @@
-HOST = "localhost"
-PORT = 6333
-COLLECTION_NAME = "live_docs"
-VECTOR_SIZE = 384  # Use the dimension of your embedding model
-ENDPOINT = "https://api.mistral.ai/v1/chat/completions"
-MISTRAL_API_KEY = "You Api Key Here"  # Replace with your actual Mistral API key
